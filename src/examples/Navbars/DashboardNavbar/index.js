542066af96d3dc58620c4aedbedfe8834e8087b8
IkigaiRamen <55280193+IkigaiRamen@users.noreply.github.com>
2023-04-29 12:44:42 +0100

    navbar changes

--- src/examples/Navbars/DashboardNavbar/index.js
+++ src/examples/Navbars/DashboardNavbar/index.js
@@ -1,5 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect,useContext } from "react";
 import { Context } from '../ContextStore';
+
+import { Navbar, NavDropdown, Nav, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 // react-router components
 import { useLocation, Link } from "react-router-dom";
 
@@ -50,6 +53,7 @@ function DashboardNavbar({ absolute, light, isMini }) {
   } = controller;
   const [openMenu, setOpenMenu] = useState(false);
   const route = useLocation().pathname.split("/").slice(1);
+  const { userData, setUserData } = useContext(Context)
 
   useEffect(() => {
     // Setting the navbar type
@@ -85,7 +89,6 @@ function DashboardNavbar({ absolute, light, isMini }) {
     setOpenConfigurator(dispatch, !openConfigurator);
   const handleOpenMenu = (event) => setOpenMenu(event.currentTarget);
   const handleCloseMenu = () => setOpenMenu(false);
-  const { userData, setUserData } = useContext(Context)
 
   // Render the notifications menu
   const renderMenu = () => (
@@ -149,15 +152,62 @@ function DashboardNavbar({ absolute, light, isMini }) {
             light={light}
           />
         </MDBox>
-        <MDBox
-          color="inherit"
-          mb={{ xs: 1, md: 1 }}
-          sx={(theme) => navbarRow(theme, { isMini })}
-        >
-          <Link to="/Market" style={{ textDecoration: "none" }}>
-          MarketPlace
-          </Link>
-        </MDBox>
+        <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        {/* <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+                    </Nav>
+                    {userData ?
+                        (<Nav>
+                            <NavLink className="nav-item" id="addButton" to="/add-product">
+                                <OverlayTrigger key="bottom" placement="bottom"
+                                    overlay={
+                                        <Tooltip id={`tooltip-bottom`}>
+                                            <strong>Add</strong>  a sell.
+                                        </Tooltip>
+                                    }
+                                > 
+                                    <BsFillPlusCircleFill />
+                                </OverlayTrigger>
+                            </NavLink>
+
+                            <NavDropdown title={<img id="navImg" src={userData.avatar} alt="user-avatar"/>} drop="left" id="collasible-nav-dropdown">
+                                <NavLink className="dropdown-item" to={`/profile/${userData._id}`}>
+                                    <BsFillPersonFill />Profile
+                                </NavLink>
+
+                                {/* <NavDropdown.Divider /> */}
+
+                                {/* <NavLink className="dropdown-item" to="/your-sells">
+                                    <BsFillGridFill />Sells
+                            </NavLink> */}
+                                <NavLink className="dropdown-item" to="/messages">
+                                    <BsFillEnvelopeFill />Messages
+                            </NavLink>
+                                {/* <NavLink className="dropdown-item" to="/wishlist">
+                                    <BsFillHeartFill />Wishlist
+                            </NavLink> */}
+
+                                <NavDropdown.Divider />
+
+                                <NavLink className="dropdown-item" to="/auth/logout" onClick={() => {
+                                    setUserData(null)
+                                }}>
+                                    <IoLogOut />Log out
+                                </NavLink>
+                            </NavDropdown>
+                        </Nav>)
+                        :
+                        (<Nav>
+                            <NavLink className="nav-item" id="nav-sign-in" to="/auth/login">
+                                Sign In
+                            </NavLink>
+                            <NavLink className="nav-item" id="nav-sign-up" to="/auth/register">
+                                Sign Up
+                            </NavLink>
+                        </Nav>)
+                    }
+                </Navbar.Collapse>
         {isMini ? null : (
           <MDBox sx={(theme) => navbarRow(theme, { isMini })}>
             <MDBox pr={1}>
